7908fb8904f2d832c18fd0cd094cee22c317b35a
William Huang <williamhuang@91app.com>
2023-12-04 10:35:09 +0800

    試試

--- Program.cs
+++ Program.cs
@@ -15,6 +15,11 @@ builder.Services.Configure<PostgresSettings>(
     builder.Configuration.GetSection("PostgresSettings")
 );
 
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    serverOptions.ListenAnyIP(5000);
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
